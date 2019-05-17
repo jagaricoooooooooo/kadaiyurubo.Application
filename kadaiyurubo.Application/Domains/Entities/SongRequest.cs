37af266b60ef6578a80449c0fff70961ac10f96b
ClosetheWorld <asterisk.jun17@gmail.com>
2019-05-18 00:42:11 +0900

    Add: Entities

---
+++ kadaiyurubo.Application/Domains/Entities/SongRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace kadaiyurubo.Application.Domains.Entities
+{
+    public class SongRequest
+    {
+        public int Level { get; set; }
+        public string Difficulty { get; set; }
+        public string Version { get; set; }
+    }
+}
