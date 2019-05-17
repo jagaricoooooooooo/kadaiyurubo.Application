884ba484d4d9bcf5b6160accf98de94cc5e5fa05
ClosetheWorld <asterisk.jun17@gmail.com>
2019-05-18 00:42:11 +0900

    Add: Entities

---
+++ kadaiyurubo.Application/Domains/Entities/SongResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace kadaiyurubo.Application.Domains.Entities
+{
+    public class SongResponse
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Difficulty { get; set; }
+        public string Version { get; set; }
+    }
+}
